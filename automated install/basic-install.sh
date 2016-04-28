f8bdc352169f6f56efb7ec10277059347eb8cd1c
bitterypaul <bitterypaul@gmail.com>
2016-04-28 22:51:11 +0530

    Update basic-install.sh

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -1,23 +1,4 @@
 #!/usr/bin/env bash
-# Pi-hole: A black hole for Internet advertisements
-# (c) 2015, 2016 by Jacob Salmela
-# Network-wide ad blocking via your Raspberry Pi
-# http://pi-hole.net
-# Installs Pi-hole
-#
-# Pi-hole is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
-# (at your option) any later version.
-
-# pi-hole.net/donate
-#
-# Install with this command (from your Pi):
-#
-# curl -L install.pi-hole.net | bash
-
-
-######## VARIABLES #########
 
 tmpLog=/tmp/pihole-install.log
 instalLogLoc=/etc/pihole/install.log
@@ -30,36 +11,8 @@ piholeFilesDir="/etc/.pihole"
 dhcpcdFile=/etc/dhcpcd.conf
 
 
-cleanupIPv6() {
-	# Removes IPv6 indicator file if we are not using IPv6
-	if [ -f "/etc/pihole/.useIPv6" ] && [ ! "$useIPv6" ]; then
-		rm /etc/pihole/.useIPv6
-	fi
-}
-
 touch /etc/pihole/.useIPv6
 #set dhcpcd.conf
-
-
-
-function valid_ip()
-{
-	local  ip=$1
-	local  stat=1
-
-	if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-		OIFS=$IFS
-		IFS='.'
-		ip=($ip)
-		IFS=$OIFS
-		[[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
-		&& ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
-		stat=$?
-	fi
-	return $stat
-}
-	piholeDNS1="8.8.8.8"
-	piholeDNS2="8.8.4.4"
 
 versionCheckDNSmasq(){
 	# Check if /etc/dnsmasq.conf is from pihole.  If so replace with an original and install new in .d directory
@@ -69,7 +22,6 @@ versionCheckDNSmasq(){
 	defaultFile="/etc/.pihole/advanced/dnsmasq.conf.original"
 	newFileToInstall="/etc/.pihole/advanced/01-pihole.conf"
 	newFileFinalLocation="/etc/dnsmasq.d/01-pihole.conf"
-
 
 	if [ -f $dnsFile1 ]; then
 		echo -n ":::    Existing dnsmasq.conf found..."
@@ -105,32 +57,10 @@ versionCheckDNSmasq(){
 		$SUDO sed -i '/^server=@DNS2@/d' $newFileFinalLocation
 	fi
 }
+cp pihole /usr/local/bin/pihole
+chmod 755 /usr/local/bin/pihole
 
-installScripts() {
-	# Install the scripts from /etc/.pihole to their various locations
-	$SUDO echo ":::"
-	$SUDO echo -n "::: Installing scripts to /opt/pihole..."
-	if [ ! -d /opt/pihole ]; then
-		$SUDO mkdir /opt/pihole
-		$SUDO chown "$USER":root /opt/pihole
-		$SUDO chmod u+srwx /opt/pihole
-	fi
-	$SUDO chmod 755 /opt/pihole/{gravity,chronometer,whitelist,blacklist,piholeLogFlush,updateDashboard,uninstall,setupLCD}.sh
-	$SUDO cp /etc/.pihole/pihole /usr/local/bin/pihole
-	$SUDO chmod 755 /usr/local/bin/pihole
-	$SUDO cp /etc/.pihole/advanced/bash-completion/pihole /etc/bash_completion.d/pihole
-	. /etc/bash_completion.d/pihole
 
-	#Tidy up /usr/local/bin directory if installing over previous install.
-	oldFiles=( gravity chronometer whitelist blacklist piholeLogFlush updateDashboard uninstall setupLCD piholeDebug)
-	for i in "${oldFiles[@]}"; do
-		if [ -f "/usr/local/bin/$i.sh" ]; then
-			$SUDO rm /usr/local/bin/"$i".sh
-		fi
-	done
-
-	$SUDO echo " done."
-}
 
 installConfigs() {
 	# Install the configs from /etc/.pihole to their various locations
@@ -206,63 +136,26 @@ CreateLogFile() {
 	fi
 }
 
-installPiholeWeb() {
-	# Install the web interface
-	$SUDO echo ":::"
-	$SUDO echo -n "::: Installing pihole custom index page..."
-	if [ -d "/var/www/html/pihole" ]; then
-		$SUDO echo " Existing page detected, not overwriting"
-	else
-		$SUDO mkdir /var/www/html/pihole
-		if [ -f /var/www/html/index.lighttpd.html ]; then
-			$SUDO mv /var/www/html/index.lighttpd.html /var/www/html/index.lighttpd.orig
-		else
-			printf "\n:::\tNo default index.lighttpd.html file found... not backing up"
-		fi
-		$SUDO cp /etc/.pihole/advanced/index.* /var/www/html/pihole/.
-		$SUDO echo " done!"
-	fi
-}
+#installPiholeWeb
+
+mkdir /var/www/html/pihole
+cp /etc/.pihole/advanced/index.* /var/www/html/pihole/.
+
 
  echo -n "::: Installing latest Cron script..."
  cp /etc/.pihole/advanced/pihole.cron /etc/cron.d/pihole
 
-
-runGravity() {
-	# Rub gravity.sh to build blacklists
-	$SUDO echo ":::"
-	$SUDO echo "::: Preparing to run gravity.sh to refresh hosts..."
-	if ls /etc/pihole/list* 1> /dev/null 2>&1; then
-		echo "::: Cleaning up previous install (preserving whitelist/blacklist)"
-		$SUDO rm /etc/pihole/list.*
-	fi
-	echo "::: Running gravity.sh"
-	$SUDO /opt/pihole/gravity.sh
-}
+sh /opt/pihole/gravity.sh
 
  useradd -r -s /usr/sbin/nologin pihole
-installPihole() {
-	# Install base files and web interface
-	checkForDependencies # done
-	stopServices
-	setUser
-	$SUDO mkdir -p /etc/pihole/
-	if [ ! -d "/var/www/html" ]; then
-		$SUDO mkdir -p /var/www/html
-	fi
-	$SUDO chown www-data:www-data /var/www/html
-	$SUDO chmod 775 /var/www/html
-	$SUDO usermod -a -G www-data pihole
-	$SUDO lighty-enable-mod fastcgi fastcgi-php > /dev/null
+installPihole()
+	mkdir -p /etc/pihole/
+	mkdir -p /var/www/html
+	chown www-data:www-data /var/www/html
+	chmod 775 /var/www/html
+	usermod -a -G www-data pihole
+	lighty-enable-mod fastcgi fastcgi-php > /dev/null
 
-	getGitFiles
-	installScripts
-	installConfigs
-	CreateLogFile
-	installPiholeWeb
-	installCron
-	runGravity
-}
 
 ######## SCRIPT ############
 # Start the installer
