dd35992cfc771b1366618051449bcc2fa081bfe7
bitterypaul <bitterypaul@gmail.com>
2016-04-28 22:25:32 +0530

    Update basic-install.sh
    Modifying it much more

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -37,130 +37,10 @@ cleanupIPv6() {
 	fi
 }
 
-use4andor6() {
-	# Let use select IPv4 and/or IPv6
-	cmd=(whiptail --separate-output --checklist "Select Protocols (press space to select)" $r $c 2)
-	options=(IPv4 "Block ads over IPv4" on
-	IPv6 "Block ads over IPv6" off)
-	choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
-	if [[ $? = 0 ]];then
-		for choice in $choices
-		do
-			case $choice in
-			IPv4  )   useIPv4=true;;
-			IPv6  )   useIPv6=true;;
-			esac
-		done
-
-		if [ $useIPv4 ] && [ ! $useIPv6 ]; then
-			getStaticIPv4Settings
-			setStaticIPv4
-			echo "::: Using IPv4 on $IPv4addr"
-			echo "::: IPv6 will NOT be used."
-		fi
-		if [ ! $useIPv4 ] && [ $useIPv6 ]; then
-			useIPv6dialog
-			echo "::: IPv4 will NOT be used."
-			echo "::: Using IPv6 on $piholeIPv6"
-		fi
-		if [ $useIPv4 ] && [  $useIPv6 ]; then
-			getStaticIPv4Settings
-			setStaticIPv4
-			useIPv6dialog
-			echo "::: Using IPv4 on $IPv4addr"
-			echo "::: Using IPv6 on $piholeIPv6"
-		fi
-		if [ ! $useIPv4 ] && [ ! $useIPv6 ]; then
-			echo "::: Cannot continue, neither IPv4 or IPv6 selected"
-			echo "::: Exiting"
-			exit 1
-		fi
-		cleanupIPv6
-	else
-		echo "::: Cancel selected. Exiting..."
-		exit 1
-	fi
-}
-
-useIPv6dialog() {
-	# Show the IPv6 address used for blocking
-	piholeIPv6=$(ip -6 route get 2001:4860:4860::8888 | awk -F " " '{ for(i=1;i<=NF;i++) if ($i == "src") print $(i+1) }')
-	whiptail --msgbox --backtitle "IPv6..." --title "IPv6 Supported" "$piholeIPv6 will be used to block ads." $r $c
-
-	$SUDO touch /etc/pihole/.useIPv6
-}
-
-getStaticIPv4Settings() {
-	# Ask if the user wants to use DHCP settings as their static IP
-	if (whiptail --backtitle "Calibrating network interface" --title "Static IP Address" --yesno "Do you want to use your current network settings as a static address?
-					IP address:    $IPv4addr
-					Gateway:       $IPv4gw" $r $c) then
-		# If they choose yes, let the user know that the IP address will not be available via DHCP and may cause a conflict.
-		whiptail --msgbox --backtitle "IP information" --title "FYI: IP Conflict" "It is possible your router could still try to assign this IP to a device, which would cause a conflict.  But in most cases the router is smart enough to not do that.
-If you are worried, either manually set the address, or modify the DHCP reservation pool so it does not include the IP you want.
-It is also possible to use a DHCP reservation, but if you are going to do that, you might as well set a static address." $r $c
-		# Nothing else to do since the variables are already set above
-	else
-		# Otherwise, we need to ask the user to input their desired settings.
-		# Start by getting the IPv4 address (pre-filling it with info gathered from DHCP)
-		# Start a loop to let the user enter their information with the chance to go back and edit it if necessary
-		until [[ $ipSettingsCorrect = True ]]
-		do
-			# Ask for the IPv4 address
-			IPv4addr=$(whiptail --backtitle "Calibrating network interface" --title "IPv4 address" --inputbox "Enter your desired IPv4 address" $r $c "$IPv4addr" 3>&1 1>&2 2>&3)
-			if [[ $? = 0 ]];then
-			echo "::: Your static IPv4 address:    $IPv4addr"
-			# Ask for the gateway
-			IPv4gw=$(whiptail --backtitle "Calibrating network interface" --title "IPv4 gateway (router)" --inputbox "Enter your desired IPv4 default gateway" $r $c "$IPv4gw" 3>&1 1>&2 2>&3)
-			if [[ $? = 0 ]];then
-				echo "::: Your static IPv4 gateway:    $IPv4gw"
-				# Give the user a chance to review their settings before moving on
-				if (whiptail --backtitle "Calibrating network interface" --title "Static IP Address" --yesno "Are these settings correct?
-					IP address:    $IPv4addr
-					Gateway:       $IPv4gw" $r $c)then
-					# If the settings are correct, then we need to set the piholeIP
-					# Saving it to a temporary file us to retrieve it later when we run the gravity.sh script
-					echo "${IPv4addr%/*}" > /tmp/piholeIP
-					echo "$piholeInterface" > /tmp/piholeINT
-					# After that's done, the loop ends and we move on
-					ipSettingsCorrect=True
-				else
-					# If the settings are wrong, the loop continues
-					ipSettingsCorrect=False
-				fi
-			else
-				# Cancelling gateway settings window
-				ipSettingsCorrect=False
-				echo "::: Cancel selected. Exiting..."
-				exit 1
-			fi
-		else
-			# Cancelling IPv4 settings window
-			ipSettingsCorrect=False
-			echo "::: Cancel selected. Exiting..."
-			exit 1
-		fi
-		done
-		# End the if statement for DHCP vs. static
-	fi
-}
-
+touch /etc/pihole/.useIPv6
 #set dhcpcd.conf
 
 
-setStaticIPv4() {
-	# Tries to set the IPv4 address
-	if grep -q "$IPv4addr" $dhcpcdFile; then
-		# address already set, noop
-		:
-	else
-		setDHCPCD
-		$SUDO ip addr replace dev "$piholeInterface" "$IPv4addr"
-		echo ":::"
-		echo "::: Setting IP to $IPv4addr.  You may need to restart after the install is complete."
-		echo ":::"
-	fi
-}
 
 function valid_ip()
 {
@@ -189,6 +69,7 @@ versionCheckDNSmasq(){
 	defaultFile="/etc/.pihole/advanced/dnsmasq.conf.original"
 	newFileToInstall="/etc/.pihole/advanced/01-pihole.conf"
 	newFileFinalLocation="/etc/dnsmasq.d/01-pihole.conf"
+
 
 	if [ -f $dnsFile1 ]; then
 		echo -n ":::    Existing dnsmasq.conf found..."
@@ -234,15 +115,6 @@ installScripts() {
 		$SUDO chown "$USER":root /opt/pihole
 		$SUDO chmod u+srwx /opt/pihole
 	fi
-	$SUDO cp /etc/.pihole/gravity.sh /opt/pihole/gravity.sh
-	$SUDO cp /etc/.pihole/advanced/Scripts/chronometer.sh /opt/pihole/chronometer.sh
-	$SUDO cp /etc/.pihole/advanced/Scripts/whitelist.sh /opt/pihole/whitelist.sh
-	$SUDO cp /etc/.pihole/advanced/Scripts/blacklist.sh /opt/pihole/blacklist.sh
-	$SUDO cp /etc/.pihole/advanced/Scripts/piholeDebug.sh /opt/pihole/piholeDebug.sh
-	$SUDO cp /etc/.pihole/advanced/Scripts/piholeLogFlush.sh /opt/pihole/piholeLogFlush.sh
-	$SUDO cp /etc/.pihole/advanced/Scripts/updateDashboard.sh /opt/pihole/updateDashboard.sh
-	$SUDO cp /etc/.pihole/automated\ install/uninstall.sh /opt/pihole/uninstall.sh
-	$SUDO cp /etc/.pihole/advanced/Scripts/setupLCD.sh /opt/pihole/setupLCD.sh
 	$SUDO chmod 755 /opt/pihole/{gravity,chronometer,whitelist,blacklist,piholeLogFlush,updateDashboard,uninstall,setupLCD}.sh
 	$SUDO cp /etc/.pihole/pihole /usr/local/bin/pihole
 	$SUDO chmod 755 /usr/local/bin/pihole
@@ -271,15 +143,6 @@ installConfigs() {
 		$SUDO mv /etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf.orig
 	fi
 	$SUDO cp /etc/.pihole/advanced/lighttpd.conf /etc/lighttpd/lighttpd.conf
-}
-
-stopServices() {
-	# Stop dnsmasq and lighttpd
-	$SUDO echo ":::"
-	$SUDO echo -n "::: Stopping services..."
-	#$SUDO service dnsmasq stop & spinner $! || true
-	$SUDO service lighttpd stop & spinner $! || true
-	$SUDO echo " done."
 }
 
 checkForDependencies() {
@@ -328,53 +191,6 @@ checkForDependencies() {
 		fi
 	done
 }
-
-getGitFiles() {
-	# Setup git repos for base files and web admin
-	echo ":::"
-	echo "::: Checking for existing base files..."
-	if is_repo $piholeFilesDir; then
-		make_repo $piholeFilesDir $piholeGitUrl
-	else
-		update_repo $piholeFilesDir
-	fi
-
-	echo ":::"
-	echo "::: Checking for existing web interface..."
-	if is_repo $webInterfaceDir; then
-		make_repo $webInterfaceDir $webInterfaceGitUrl
-	else
-		update_repo $webInterfaceDir
-	fi
-}
-
-is_repo() {
-	# If the directory does not have a .git folder it is not a repo
-	echo -n ":::    Checking $1 is a repo..."
-		if [ -d "$1/.git" ]; then
-		echo " OK!"
-		return 1
-	fi
-	echo " not found!!"
-	return 0
-}
-
-make_repo() {
-    # Remove the non-repod interface and clone the interface
-    echo -n ":::    Cloning $2 into $1..."
-    $SUDO rm -rf "$1"
-    $SUDO git clone -q "$2" "$1" > /dev/null & spinner $!
-    echo " done!"
-}
-
-update_repo() {
-    # Pull the latest commits
-    echo -n ":::     Updating repo in $1..."
-    cd "$1" || exit
-    $SUDO git pull -q > /dev/null & spinner $!
-    echo " done!"
-}
-
 
 CreateLogFile() {
 	# Create logfiles if necessary
