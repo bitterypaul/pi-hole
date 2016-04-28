adfb961d561eff57364c75a92cbaf150670f9d41
bitterypaul <bitterypaul@gmail.com>
2016-04-28 22:50:43 +0530

    Update install.sh

--- automated install/install.sh
+++ automated install/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 sudo bash
-dnsutils bc toilet figlet dnsmasq lighttpd php5-common php5-cgi php5 git curl unzip wget
+apt-get install dnsutils bc toilet figlet dnsmasq lighttpd php5-common php5-cgi php5 git curl unzip wget
