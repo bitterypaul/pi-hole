1def7cca022f0d7ac8bc21558bc25a35df833215
bitterypaul <bitterypaul@gmail.com>
2016-04-29 17:57:00 +0530

    Update uninstall.sh
    Refactor#2

--- automated install/uninstall.sh
+++ automated install/uninstall.sh
@@ -1,6 +1,7 @@
 
 sudo bash
 
+
 touch /etc/pihole/.useIPv6
 #set interface as wlan0.i.e, modify etc/network/interfaces.
 #getstaticipv4settings.i.e, copy dhcpcd.conf to req location.
@@ -9,6 +10,7 @@ touch /etc/pihole/.useIPv6
 #
 #
 #
+apt-get update
 
 apt-get install dnsutils bc toilet figlet dnsmasq lighttpd php5-common php5-cgi php5 git curl unzip wget
 
@@ -18,6 +20,8 @@ service dnsmasq stop
 
 #add user pihole
 useradd -r -s /usr/sbin/nologin pihole
+
+
 
 #continue the same logic as in installPihole()
 mkdir -p /etc/pihole/
@@ -34,9 +38,22 @@ lighty-enable-mod fastcgi fastcgi-php
 ##edit:git repos for pi-hole cannot be used since we don't declare variables.
 #copy n paste only.
  
+#installscripts()
+mkdir /opt/pihole
+#beware of variable user!!!!!!!!!!!!!!!!
+chown "$USER":root /opt/pihole
+chmod u+srwx /opt/pihole
 
-
-
+cp Scripts/gravity.sh /opt/pihole/gravity.sh
+cp Scripts/chronometer.sh /opt/pihole/chronometer.sh
+cp Scripts/whitelist.sh /opt/pihole/whitelist.sh
+cp Scripts/blacklist.sh /opt/pihole/blacklist.sh
+cp Scripts/piholeDebug.sh /opt/pihole/piholeDebug.sh
+cp Scripts/piholeLogFlush.sh /opt/pihole/piholeLogFlush.sh
+cp Scripts/updateDashboard.sh /opt/pihole/updateDashboard.sh
+chmod 755 /opt/pihole/{gravity,chronometer,whitelist,blacklist,piholeLogFlush,updateDashboard}.sh
+cp /etc/.pihole/pihole /usr/local/bin/pihole
+chmod 755 /usr/local/bin/pihole
 
 
 
